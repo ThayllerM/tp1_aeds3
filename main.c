fe8e503a84fdc4fe9b56c3092c44a4c953734278
Festeao <cadefm@hotmail.com>
2019-11-26 20:52:31 -0300

    get opt ajeitado

--- main.c
+++ main.c
@@ -1,34 +1,31 @@
 #include "tp.h"
 
 int main (int argc, char *argv[]) {
-    int opt;
+    int opt = getopt(argc, argv, "a:");
     int algoritmo_escolhido, i;
 
-    while((opt = getopt(argc, argv, "a:")) > 0) {
-        switch (opt) {
-            case 'a':
-                if (strcmp(optarg, "FB")) { //algoritmo forca bruta
-                    algoritmo_escolhido = 1;
-                }
-                else if (strcmp(optarg, "AG")) { //algoritmo guloso
-                    algoritmo_escolhido = 2;
-                }
-                else if (strcmp(optarg, "PD")) { //algoritmo programacao dinamica
-                    algoritmo_escolhido = 3;
-                }
-                else{
-                    fprintf(stderr, "Opcao invalida\n");
-                    return -1;
-                }
-                break;
-            default:
-                fprintf(stderr, "Opcao invalida ou faltando argumento: `-a'\n") ;
+    switch (opt) {
+        case 'a':
+            if (optarg[0] == 'F' && optarg[1] == 'B') { //algoritmo forca bruta
+                algoritmo_escolhido = 1;
+            }
+            else if (optarg[0] == 'A' && optarg[1] == 'G') { //algoritmo guloso
+                algoritmo_escolhido = 2;
+            }
+            else if (optarg[0] == 'P' && optarg[1] == 'D') { //algoritmo programacao dinamica
+                algoritmo_escolhido = 3;
+            }
+            else{
+                fprintf(stderr, "Opcao invalida\n");
                 return -1;
-        }
+            }
+            break;
+        default:
+            fprintf(stderr, "Opcao invalida ou faltando argumento: '-a'\n") ;
+            return -1;
     }
 
     int **matriz_instancias = abri_arq("entrada.txt");
-    algoritmo_escolhido = 3;
     int vet_bin[500] = {0};
 
     switch (algoritmo_escolhido) {
